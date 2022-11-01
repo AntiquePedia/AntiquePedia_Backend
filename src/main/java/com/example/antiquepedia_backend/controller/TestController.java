dd0e3a7cd6462a21227c5bc0d9b7bf19f2cd7051
langzishuai <1336954404@qq.com>
2022-11-01 17:24:02 +0800

    add test_controller

---
+++ src/main/java/com/example/antiquepedia_backend/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.antiquepedia_backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String HelloSam(){
+        return "Hello Sam!";
+    }
+}

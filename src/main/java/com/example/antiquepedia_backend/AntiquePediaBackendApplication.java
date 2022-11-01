d8100eb9ed88e496fd83c5ddb30f89acb290f472
SamWang <1336954404@qq.com>
2022-11-01 16:47:42 +0800

    upload base codes

---
+++ src/main/java/com/example/antiquepedia_backend/AntiquePediaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.antiquepedia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiquePediaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiquePediaBackendApplication.class, args);
+    }
+
+}

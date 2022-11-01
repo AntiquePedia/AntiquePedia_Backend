15d3d8d9a169ff6ba1411cd1e89be7d0b077889d
SamWang <1336954404@qq.com>
2022-11-01 16:17:55 +0800

    upload backend code

---
+++ AntiquePedia_Backend/src/test/java/com/example/antiquepedia_backend/AntiquePediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.antiquepedia_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiquePediaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

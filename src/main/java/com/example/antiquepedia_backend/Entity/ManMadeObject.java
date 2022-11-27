cec299eebed9facc904190e70df347dace62224d
langzishuai <1336954404@qq.com>
2022-11-27 20:28:24 +0800

    modify ManMadeObject

--- src/main/java/com/example/antiquepedia_backend/Entity/ManMadeObject.java
+++ src/main/java/com/example/antiquepedia_backend/Entity/ManMadeObject.java
@@ -1,4 +1,13 @@
 package com.example.antiquepedia_backend.Entity;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class ManMadeObject {
+    private String URI;
+    private List<String> labels;
+    private List<String> representations;
+    private List<String> refers;
 }

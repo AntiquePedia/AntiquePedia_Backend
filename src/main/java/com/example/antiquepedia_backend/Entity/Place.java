f3fd0edc678ccb6524f273f1a8c4675c4d347c97
langzishuai <1336954404@qq.com>
2022-11-29 09:43:52 +0800

    modify Place

--- src/main/java/com/example/antiquepedia_backend/Entity/Place.java
+++ src/main/java/com/example/antiquepedia_backend/Entity/Place.java
@@ -1,4 +1,9 @@
 package com.example.antiquepedia_backend.Entity;
 
 public class Place {
+    private String uri;
+    private String label;
+    private String fallWithin;
+    private String longitude;
+    private String latitude;
 }
